0e4851eec3bb67b31f5aca7eaaf666ca29a476ba
abelcq <>
2021-05-03 15:53:44 +0000

    ordenando archivos por carpetas

---
+++ distint-languaje/AreaTriangulo.py
@@ -0,0 +1,9 @@
+#Definicion de variables y otros
+print("Ejercicio 01: Area de un triangulo")
+#Datos de entrada mediante dispositivos de entrada
+b=int(input("Baseeeee:"))
+h=int(input("Alturaaa:"))
+#Proceso
+Area=(b*h)/2
+#Datos de salida en dispositivos de salida
+print("Area del triangulo es:", Area)
