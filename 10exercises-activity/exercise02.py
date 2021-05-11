7ca5fc7f4b786d532c5313924e1b64579ada036f
abelcq <>
2021-05-11 02:33:32 +0000

    jklñjklñjklñ

--- 10exercises-activity/exercise02.py
+++ 10exercises-activity/exercise02.py
@@ -1,8 +1,6 @@
 #exercise 3.1
-edadP=int(input("Ingrese la edad que tiene:"))
-if edadP>=18:
-    mensaje ="Usted tiene la edad necesaria para votar"
+e = int(input("Ingrese la edad que tiene:"))
+if e >=18:
+    print("Usted tiene la edad necesaria para votar")
 else:
-    mensaje ="Usted no cumple con la edad mínima para votar"
-print(mensaje)
-#VotoElecciones()
+    print("Usted no cumple con la edad mínima para votar")
